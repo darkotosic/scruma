e4703912eeeb08df6d8f70b529e1d9a3dc387842
Darko Tosic <darkotosic1986@gmail.com>
2026-02-21 15:00:54 +0100

    Add HomeStatusView to root URL configuration

--- scruma-api/config/urls.py
+++ scruma-api/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from core.views import HomeStatusView
 
 urlpatterns = [
+    path("", HomeStatusView.as_view()),  # ROOT STATUS
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
