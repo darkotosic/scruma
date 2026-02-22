c0d205f0bdc444c35925be483367c5efb8495208
Darko Tosic <darkotosic1986@gmail.com>
2026-02-22 02:35:08 +0100

    Add v1 Django API endpoints for home, posts, announcements

--- scruma-api/core/urls.py
+++ scruma-api/core/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from .views import AnnouncementListView, HealthView, HomePageView
+from .views import (
+    AnnouncementListView,
+    HealthView,
+    HomePageView,
+    V1AnnouncementsView,
+    V1HomeView,
+    V1PostsView,
+)
 
 urlpatterns = [
     path("health/", HealthView.as_view(), name="health"),
     path("announcements/", AnnouncementListView.as_view(), name="announcements"),
     path("home/", HomePageView.as_view(), name="home"),
+    # v1
+    path("v1/home/", V1HomeView.as_view(), name="v1-home"),
+    path("v1/posts/", V1PostsView.as_view(), name="v1-posts"),
+    path("v1/announcements/", V1AnnouncementsView.as_view(), name="v1-announcements"),
 ]
